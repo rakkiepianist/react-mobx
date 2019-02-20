77639d67036b127801aa062710fb28181cfac062
Van Linh <rakkiepianist@gmail.com>
2019-02-20 16:43:41 +0700

    Init Project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,117 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { configure, decorate, observable, action, computed } from 'mobx';
+import { observer } from 'mobx-react';
+configure({ enforceActions: 'observed' });
+
+class Store {
+    employeesList = [
+        { name: 'John Doe', salary: 150 },
+        { name: 'Richard Roe', salary: 225 }
+    ];
+
+    clearList() {
+        this.employeesList = [];
+    }
+
+    pushEmployee(e) {
+        this.employeesList.push(e);
+    }
+
+    get totalSum() {
+        let sum = 0;
+        this.employeesList.map(e => (sum = sum + e.salary));
+        return sum;
+    }
+
+    get highEarnersCount() {
+        return this.employeesList.filter(e => e.salary > 500).length;
+    }
+}
+
+decorate(Store, {
+    employeesList: observable,
+    clearList: action,
+    pushEmployee: action,
+    totalSum: computed,
+    highEarnersCount: computed
+});
+
+const appStore = new Store();
+
+const Row = props => {
+    return (
+        <tr>
+            <td>{props.data.name}</td>
+            <td>{props.data.salary}</td>
+        </tr>
+    );
+};
+
+class Table extends Component {
+    render() {
+        const { store } = this.props;
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Name:</td>
+                            <td>Daily salary:</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {store.employeesList.map((e, i) => (
+                            <Row key={i} data={e} />
+                        ))}
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td>TOTAL:</td>
+                            <td>{store.totalSum}</td>
+                        </tr>
+                    </tfoot>
+                </table>
+                <div className="fade">
+                    You have <u>{store.highEarnersCount} team members </u>that
+                    earn more than 500$/day.
+                </div>
+            </div>
+        );
+    }
+}
+Table = observer(Table);
+
+class Controls extends Component {
+    addEmployee = () => {
+        const name = prompt('The name:');
+        const salary = parseInt(prompt('The salary:'));
+        this.props.store.pushEmployee({ name, salary });
+    };
+
+    clearList = () => {
+        this.props.store.clearList();
+    };
+
+    render() {
+        return (
+            <div className="controls">
+                <button onClick={this.clearList}>clear table</button>
+                <button onClick={this.addEmployee}>add record</button>
+            </div>
+        );
+    }
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Mobx Table</h1>
+                <Controls store={appStore} />
+                <Table store={appStore} />
+            </div>
+        );
+    }
 }
 
 export default App;
