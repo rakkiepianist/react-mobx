f04ad849546aaf4aebcfd306dc3226689858bcac
Van Linh <rakkiepianist@gmail.com>
2019-02-20 16:49:10 +0700

    Updates

---
+++ precache-manifest.0b5abb3ed7d73a56dbd60dd97cf830d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7b8df15a3a461663414",
+    "url": "/react-mobx/static/js/runtime~main.a7b8df15.js"
+  },
+  {
+    "revision": "b33d493afe767161e077",
+    "url": "/react-mobx/static/js/main.b33d493a.chunk.js"
+  },
+  {
+    "revision": "a25afa4d888ce6cc851c",
+    "url": "/react-mobx/static/js/2.a25afa4d.chunk.js"
+  },
+  {
+    "revision": "b33d493afe767161e077",
+    "url": "/react-mobx/static/css/main.4c40ebc7.chunk.css"
+  },
+  {
+    "revision": "7e3dc1164d3609105dbe9c59a230bbe1",
+    "url": "/react-mobx/index.html"
+  }
+];
